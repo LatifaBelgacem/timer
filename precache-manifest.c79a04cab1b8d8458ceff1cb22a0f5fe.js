2a092786cfd3c1281d027d991f53c390e02611ab
LatifaBelgacem <latifabelgacem320@gmail.com>
2019-08-16 10:36:59 +0100

    Updates

---
+++ precache-manifest.c79a04cab1b8d8458ceff1cb22a0f5fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d4b877397a8311cbc377433c0449b5",
+    "url": "/timer/index.html"
+  },
+  {
+    "revision": "66457176c7531dba7ffa",
+    "url": "/timer/static/css/main.afa36794.chunk.css"
+  },
+  {
+    "revision": "67cd0a63852c9f142a9e",
+    "url": "/timer/static/js/2.f8ed4eb6.chunk.js"
+  },
+  {
+    "revision": "66457176c7531dba7ffa",
+    "url": "/timer/static/js/main.953bd1c8.chunk.js"
+  },
+  {
+    "revision": "890205034837c931c545",
+    "url": "/timer/static/js/runtime~main.2c670c97.js"
+  }
+]);
